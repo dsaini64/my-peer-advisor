02777aeded93e8cf357b844600c629db03686f87
DiegoMeseguer <DiegoMeseguer@users.noreply.github.com>
2023-10-29 22:12:41 -0700

    Added Professor schema

---
+++ professor_schema.js
@@ -0,0 +1,16 @@
+// Defines the schema for the Professors Collection
+// Tags and Rating fields are empty by default
+// Everything else is required
+
+const mongoose = require('mongoose');
+
+const professorSchema = new mongoose.Schema({
+    professorName: {type: String, required: true},
+    department: {type: String, required: true},
+    background: {type: String, required: true},
+    tags: {type: [String], default: []},
+    rating: {type: Number, default: null}
+});
+
+module.exports = mongoose.model("Professor", professorSchema);
+
