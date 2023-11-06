5b782029de14873bd708ef31b3c190c2562342fc
Diego <meseguer_diego@tutanota.com>
2023-11-05 23:10:39 -0800

    Added new tag schema and additional fields to other schemas

--- server/routes/professors.js
+++ server/routes/professors.js
@@ -7,11 +7,12 @@ router.get("/", async (req, res) => {
     console.log(req.query.search);
     const searchName = req.query.search;
     try {
-        const result = await Professor.find({ name: {$regex: searchName, $options:'i'} }).exec();
+        const result = await Professor.find({ name: {$regex: searchName, $options:'i'} },
+                                            ).exec();
         if(Object.keys(result).length === 0) {
-            res.status(404).send('<h2> No professor matches your search</h2>');
+            res.status(200).send('<h2> No professor matches your search</h2>');
         } else {
-            res.send(result);
+            res.json(result);
         }
     } catch (err) {
         res.status(500).send('<h2>Internal Server Error</h2>');
