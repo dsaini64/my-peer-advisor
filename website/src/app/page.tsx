24e12a09795b09549453dcb7b2bec4a215b986c4
tmishra17 <jetmi@live.com>
2023-11-08 16:46:50 -0800

    Updating branch to current state

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Homepage() {
 function SearchBar() {
   return (
     <div className="search-bar">
-      <input type="text" placeholder="Search..." />
+      <input type="text" placeholder="Search for class/professor.." />
       <button>Search</button>
     </div>
   );
