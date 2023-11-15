9b7226215384b8ef56af67b10dce8026b6f8e239
Zach <zacharyaclark@gmail.com>
2023-11-15 15:40:21 -0800

    starting search functionality

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,22 +1,47 @@
-import Image from 'next/image'
-import React from 'react';
+"use client"
+import React, { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
-export default function Homepage() {
+
+const Page: React.FC = () => {
+
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+
   return (
     <div>
       <div className='MyPeerAdvisorHeader'>
-        <h1 >My Peer Advisor</h1>
+        <h1>My Peer Advisor</h1>
       </div>
-      <SearchBar />
+      <SearchBar searchTerm={searchTerm} onInputChange={handleInputChange} />
+      <Link href={`/search-results?query=${searchTerm}`}>
+        <>Search</>
+      </Link>
     </div>
   );
+};
+
+interface SearchBarProps {
+  searchTerm: string;
+  onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-function SearchBar() {
+const SearchBar: React.FC<SearchBarProps> = ({ searchTerm, onInputChange }) => {
   return (
     <div className="search-bar">
-      <input type="text" placeholder="Search for class/professor.." />
-      <button>Search</button>
+      <input
+        type="text"
+        placeholder="Search for class/professor.."
+        value={searchTerm}
+        onChange={onInputChange}
+      />
     </div>
   );
-}
+};
+
+export default Page;
