4de05012f6c31d3489b87aff3deb3f9ab5c65e03
tmishra17 <jetmi@live.com>
2023-11-21 15:20:24 -0800

    Finished professor card

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -23,7 +23,7 @@
 // }
 
 import { useState, useEffect } from "react";
-import { Button } from "antd";
+import { Button, Typography } from "antd";
 
 export default function ProfilePage() {
   // call backend for data
@@ -31,14 +31,29 @@ export default function ProfilePage() {
   let profDesc = "Background: Phd in Computer Science. Expert in Machine Learning and Artificial Intelligence. Worked for NASA."
   let tags = "Caring, Inspirational, Funny"
   let degree  = "Computer Science"
-  let ratings = 32
+  let ratings = "32"
+  let ratingNum = "7.8"
+  let studentRatingNum = 5
+  let userRatingNum = ["9.0", "6.5", "8.3", "7.3"]
+  let userCourseName = "CSCI 187"
+  let userDesc = "Umm Kulthum’s rise to fame was a very interesting journey. She was born on December 31st 1898, and was raised in a very small village in Egypt. Her family was very poor so she never had much growing up. The one thing she did had, which is also the thing that would carry her to fame, was her incredible voice. During the years Kulthum was growing up as a young women, most of the contemporary Arabic singers at the time were male. In fact, most of the singers she looked up to were male as well. Women played mostly a secondary role in regards to music, but things were much different for Kulthum. Her incredible voice made her stand out substantially from not only female singers but males singers as well. Many people who have heard Kulthum’s voice live, have commented on its beauty and fullness. Although gender roles were very much prominent during the years she was alive, it didn’t really seem to apply to her. Many people loved Umm Kulthum and her music."
+  let userTags = "ready for class, lots of reading, caring, wants you to learn"
   return (
-    <div className="card">
-        <div className="card-body profileCardLayout">
-          
-            <Rating profName={profName} reviewNum={ratings}/>
-            <ProfileBody profName={profName} profDesc={profDesc} tags={tags} degree={degree}/>
+    <div className="profilePageLayout" >
+      <ProfCard tags={tags} degree={degree} profDesc={profDesc} profName={profName} ratings={ratings} ratingNum={ratingNum} />
+      <div>{studentRatingNum} User Reviews</div>
+      <div>
+        <div className="card">
+          <div className="card-body profileCardLayout">
+            <UserRating ratingNum={userRatingNum[0]}/>
+            <UserBody userCourseName={userCourseName} userDesc={userDesc} userTags={userTags}/>
+          </div>
         </div>
+        <div>User review card</div>
+        <div>User review card</div>
+        <div>User review card</div>
+        <div>User review card</div>
+      </div>
     </div>
   );  
 }
@@ -62,27 +77,105 @@ function ProfileBody(content: profileBodyProps) {
 }
 
 type profInfoProp = {
-  profName: string
-  reviewNum: number
+  profName: string,
+  reviewNum: string,
+  ratingNum: string
 }
 
-function Rating(profInfo: profInfoProp){
+function Rating({profName, reviewNum, ratingNum}: profInfoProp){
   // call backend function for rating
   return (
     <div className="ratingLayout">
-      <div className="ratingHeader">
-      <div className="ratingText">
-        Rating
-      </div>
-      <div className="ratingNumberText">
-        7.8{/* computed average */}
-      </div>
-      <div className="reviewNumText"> {profInfo.reviewNum} ratings</div> 
-    </div>
+      <RatingHeader reviewNum={reviewNum} ratingNum={ratingNum}/>
     <div className="ratingFooter">
-      <Button type="link">Rate {profInfo.profName}</Button>
+      <Button type="link" className="align-self-end">Rate {profName}</Button>
       {/* basically rating footer, should maybe add more components to make it more clear what each thing does */}
       </div>
     </div>
   );
+}
+
+type ratingHeaderProps = {
+  reviewNum: string,
+  ratingNum: string
+}
+
+function RatingHeader({reviewNum, ratingNum}:ratingHeaderProps){
+  return (
+    <div className="ratingHeader">
+      <div className="ratingText">
+        Rating
+      </div>
+      <div className="ratingNumberText">
+        {ratingNum}
+      </div>
+      <div className="reviewNumText"> {reviewNum} ratings</div> 
+    </div>
+  )
+}
+
+type userRatingProps = {
+  ratingNum: string
+}
+
+function UserRating({ratingNum}: userRatingProps) {
+  return (
+    <div>
+        <UserRatingHeader ratingNum={ratingNum}/>
+    </div>
+  )
+}
+
+type userRatingHeaderProps = {
+  ratingNum: string
+}
+
+function UserRatingHeader({ratingNum}:userRatingHeaderProps) {
+  return (
+  <div className="userRatingLayout">
+   <div className="userRatingHeader">
+      <div className="userRatingText">
+        Rating
+      </div>
+      <div className="ratingNumberText">
+        {ratingNum}{/* computed average */}
+      </div>
+    </div>
+</div>)
+}
+
+type userBodyProps = {
+  userDesc: string,
+  userTags: string,
+  userCourseName: string
+}
+
+function UserBody({userDesc, userTags, userCourseName}:userBodyProps) {
+  return (
+    <div className="profileBody">
+      <div><h1>{userCourseName}</h1></div>
+      <div><p>{userDesc}</p></div>
+      <div>Top Tags: {userTags}</div>
+    </div>
+  )
+}
+
+type ProfCardProps = {
+  profName: string,
+  profDesc: string,
+  tags: string,
+  degree: string,
+  ratings: string,
+  ratingNum: string
+}
+
+function ProfCard({profName, ratings, ratingNum, degree, tags, profDesc}:ProfCardProps) {
+  return (
+    <div className="card">
+    <div className="card-body profileCardLayout">
+        <Rating profName={profName} reviewNum={ratings} ratingNum={ratingNum}/>
+        <ProfileBody profName={profName} profDesc={profDesc} tags={tags} degree={degree}/>
+    </div>
+</div>
+  )
 }
