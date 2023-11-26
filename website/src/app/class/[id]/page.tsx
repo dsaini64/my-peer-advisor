de5bf5f549556821cfc6eea0819297750600e6b9
Zach <zacharyaclark@gmail.com>
2023-11-25 23:38:42 -0800

    formatting class page

--- website/src/app/class/[id]/page.tsx
+++ website/src/app/class/[id]/page.tsx
@@ -1,21 +1,26 @@
 "use client"
 
+"use client"
+
 import { useState, useEffect } from 'react'
 
 export default function ClassPage({ params }: { params: { id: string } }) {
 
   const [data, setData] = useState<null | any>(null)
   const [isLoading, setLoading] = useState(true)
+  const [data, setData] = useState<null | any>(null)
+  const [isLoading, setLoading] = useState(true)
 
-    useEffect(() => {
-        fetch(`http://localhost:3000/api/course/${params.id}`)
-            .then(res => res.json())
-            .then(data => {
-                setData(data)
-                setLoading(false)
-            })
-    }, [])
+  useEffect(() => {
+    fetch('http://localhost:9080/api/v1/courses/655e740644c47b2420f26ce5/reviews')
+      .then(res => res.json())
+      .then(data => {
+        setData(data)
+        setLoading(false)
+      })
+  }, [])
 
+  console.log(data);
   console.log(data);
 
   return (
@@ -29,6 +34,19 @@ function ClassProfile(content: any) {
         <div>
           <h1 className='font-bold'>{content.course.classCode}</h1> <h1 >{content.course.courseName}</h1>
         </div>
+  return (
+    (data === null) ? <div>Loading...</div> : ClassProfile(data)
+  );
+}
+
+function ClassProfile(content: any) {
+  return (
+    <div className="flex-col h-screen w-screen">
+        <div>
+          <h1 className='font-bold'>{content.course.classCode}</h1> <h1 >{content.course.courseName}</h1>
+        </div>
       </div>
+  )
+}
   )
 }
