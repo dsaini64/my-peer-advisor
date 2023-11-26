d1945d8250bc95b09373471eeff242bf31dcfb02
Zach <zacharyaclark@gmail.com>
2023-11-25 23:38:42 -0800

    formatting class page

--- website/src/app/searchPage/page.tsx
+++ website/src/app/searchPage/page.tsx
@@ -1,164 +1,136 @@
-import { useState, useEffect } from "react";
-import { Button, Typography, } from "antd";
+"use client"
 
-const { Paragraph } = Typography
+import Image from 'next/image'
+import React from 'react';
+//import { Button, Typography, } from "antd";
+import { useState } from "react";
+import { useRouter } from 'next/navigation';
 
-export default function ProfilePage() {
-  // call backend for data
-  let profName = "Michael Stravinsky"
-  let profDesc = "Background: Phd in Computer Science. Expert in Machine Learning and Artificial Intelligence. Worked for NASA."
-  let tags = "Caring, Inspirational, Funny"
-  let degree  = "Computer Science"
-  let ratings = "32"
-  let ratingNum = "7.8"
-  let studentRatingNum = 5
-  let userRatingNum = ["9.0", "6.5", "8.3", "7.3"]
-  let userCourseName = "CSCI 187"
-  let userDesc = "Umm Kulthum’s rise to fame was a very interesting journey. She was born on December 31st 1898, and was raised in a very small village in Egypt. Her family was very poor so she never had much growing up. The one thing she did had, which is also the thing that would carry her to fame, was her incredible voice. During the years Kulthum was growing up as a young women, most of the contemporary Arabic singers at the time were male. In fact, most of the singers she looked up to were male as well. Women played mostly a secondary role in regards to music, but things were much different for Kulthum. Her incredible voice made her stand out substantially from not only female singers but males singers as well. Many people who have heard Kulthum’s voice live, have commented on its beauty and fullness. Although gender roles were very much prominent during the years she was alive, it didn’t really seem to apply to her. Many people loved Umm Kulthum and her music."
-  let userTags = "ready for class, lots of reading, caring, wants you to learn"
-  return (
-    <div className="profilePageLayout" >
-      <ProfCard tags={tags} degree={degree} profDesc={profDesc} profName={profName} ratings={ratings} ratingNum={ratingNum} />
-      <div className="numOfUserReviews">{studentRatingNum} User Reviews</div>
+export default function SearchPage() {
+    return (
       <div>
-        <UserCard userCourseName={userCourseName} userDesc={userDesc} userTags={userTags} ratingNum={userRatingNum[0]} />
-        <UserCard userCourseName={userCourseName} userDesc={userDesc} userTags={userTags} ratingNum={userRatingNum[1]} />
-        <UserCard userCourseName={userCourseName} userDesc={userDesc} userTags={userTags} ratingNum={userRatingNum[2]} />
-        <UserCard userCourseName={userCourseName} userDesc={userDesc} userTags={userTags} ratingNum={userRatingNum[3]} />
+        <h1>My Peer Advisor</h1>
+        <SearchBar2 />
+        <ResultList />
       </div>
-    </div>
-  );  
+    );
 }
 
 type profileBodyProps = {
-  profName: string,
-  profDesc: string,
-  degree: string,
-  tags: string
+    profName: string,
+    profDesc: string,
+    degree: string,
+    tags: string
 }
-
+  
 function ProfileBody(content: profileBodyProps) {
-  return (
-    <div className="profileBody">
-      <div><h1>{content.profName}</h1></div>
-      <div>{content.degree}</div>
-      <div><p>{content.profDesc}</p></div>
-      <div>Top Tags: {content.tags}</div>
-    </div>
-  )
+    return (
+      <div className="profileBody">
+        <div><h1>{content.profName}</h1></div>
+        <div>{content.degree}</div>
+        <div><p>{content.profDesc}</p></div>
+        <div>Top Tags: {content.tags}</div>
+      </div>
+    )
 }
 
 type profInfoProp = {
-  profName: string,
-  reviewNum: string,
-  ratingNum: string
-}
-
+    profName: string,
+    reviewNum: string,
+    ratingNum: string
+  }
+  
 function Rating({profName, reviewNum, ratingNum}: profInfoProp){
-  // call backend function for rating
-  return (
-    <div className="ratingLayout">
-      <RatingHeader reviewNum={reviewNum} ratingNum={ratingNum}/>
-    <div className="ratingFooter">
-      <Button type="link" className="align-self-end">Rate {profName}</Button>
-      {/* basically rating footer, should maybe add more components to make it more clear what each thing does */}
+    // call backend function for rating
+    return (
+      <div className="ratingLayout">
+        <RatingHeader reviewNum={reviewNum} ratingNum={ratingNum}/>
+        <div className="ratingFooter">
+            
+            {/* basically rating footer, should maybe add more components to make it more clear what each thing does */}
+        </div>
       </div>
-    </div>
-  );
+    );
 }
 
 type ratingHeaderProps = {
-  reviewNum: string,
-  ratingNum: string
+    reviewNum: string,
+    ratingNum: string
 }
-
+  
 function RatingHeader({reviewNum, ratingNum}:ratingHeaderProps){
-  return (
-    <div className="ratingHeader">
-      <div className="ratingText">
-        Rating
-      </div>
-      <div className="ratingNumberText">
-        {ratingNum}
-      </div>
-      <div className="reviewNumText"> {reviewNum} ratings</div> 
-    </div>
-  )
-}
-
-type userRatingProps = {
-  ratingNum: string
-}
-
-function UserRating({ratingNum}: userRatingProps) {
-  return (
-      <div className="userRatingLayout">
-       <div className="userRatingHeader">
-          <div className="userRatingText">
-            Rating
-          </div>
-          <div className="userRatingNumberText">
-            {ratingNum}
-          </div>
+    return (
+      <div className="ratingHeader">
+        <div className="ratingText">
+          Rating
         </div>
-    </div>)
-}
-
-type userBodyProps = {
-  userDesc: string,
-  userTags: string,
-  userCourseName: string
-}
-
-function UserBody({userDesc, userTags, userCourseName}:userBodyProps) {
-  return (
-    <div className="userProfileBody">
-      <div><h1>{userCourseName}</h1></div>
-      <div><p>{userDesc}</p></div>
-      <div>Top Tags: {userTags}</div>
-    </div>
-  )
+        <div className="ratingNumberText">
+          {ratingNum}
+        </div>
+        <div className="reviewNumText"> {reviewNum} ratings</div> 
+      </div>
+    )
 }
 
 type ProfCardProps = {
-  profName: string,
-  profDesc: string,
-  tags: string,
-  degree: string,
-  ratings: string,
-  ratingNum: string
+    profName: string,
+    profDesc: string,
+    tags: string,
+    degree: string,
+    ratings: string,
+    ratingNum: string
 }
-
+  
 function ProfCard({profName, ratings, ratingNum, degree, tags, profDesc}:ProfCardProps) {
-  return (
-    <div className="card">
-    <div className="card-body profileCardLayout">
-        <Rating profName={profName} reviewNum={ratings} ratingNum={ratingNum}/>
-        <ProfileBody profName={profName} profDesc={profDesc} tags={tags} degree={degree}/>
-    </div>
-</div>
-  )
+    const router = useRouter();
+
+    return (
+        <div   
+            onClick={() => {
+                router.push('/profilePage?id=123'); // should be variable later on based on prof id from database
+            }}
+            className="card">
+            <div className="card-body profileCardLayout">
+                <Rating profName={profName} reviewNum={ratings} ratingNum={ratingNum}/>
+                <ProfileBody profName={profName} profDesc={profDesc} tags={tags} degree={degree}/>
+            </div>
+        </div>
+    )
 }
 
-type UserCardProps = {
-  userCourseName: string,
-  userDesc: string,
-  userTags: string,
-  ratingNum: string
-}
+function SearchBar2() {
+    const router = useRouter();
+    const [search, setSearch] = useState("");
 
-function UserCard({userCourseName, userDesc, userTags, ratingNum}: UserCardProps){
-  return (
-    <div className="card">
-      <div className="card-body profileCardLayout">
-        <UserRating ratingNum={ratingNum}/>
-        <UserBody userCourseName={userCourseName} userDesc={userDesc} userTags={userTags}/>
+    return (
+      <div className="search-bar-2">
+        <input 
+            onChange={(e) => setSearch(e.target.value)}
+            type="text" 
+            value={search} 
+            placeholder="Search..." />
+        <button
+            onClick={() => {
+                router.push('/searchPage?q=' + search);
+            }}
+        >Search</button>
       </div>
-    </div>
-  )
+    );
 }
 
-//implement show more button first for cards, then for text, 
-// maybe use text first to build understanding
+function ResultList() {
+    // need to get from database
+    let profName = "Michael Stravinsky"
+    let profDesc = "Background: Phd in Computer Science. Expert in Machine Learning and Artificial Intelligence. Worked for NASA."
+    let tags = "Caring, Inspirational, Funny"
+    let degree  = "Computer Science"
+    let ratings = "32"
+    let ratingNum = "7.8"
 
-// need to resolve routing issue
+    return (
+        <div>
+            <ProfCard tags={tags} degree={degree} profDesc={profDesc} profName={profName} ratings={ratings} ratingNum={ratingNum} />
+            <ProfCard tags={"Unfunny"} degree={"Computer Engineering"} profDesc={"Background: None"} profName={"Freddy Fazbear"} ratings={"9"} ratingNum={"9.9"} />
+        </div>
+    )
+}
