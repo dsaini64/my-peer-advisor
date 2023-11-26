458d0a9215a1e396718e4a788ecaf3c24e829375
Zach <zacharyaclark@gmail.com>
2023-11-25 19:31:36 -0800

    working on api request

--- website/src/app/class/[id]/page.tsx
+++ website/src/app/class/[id]/page.tsx
@@ -8,13 +8,12 @@ export default function Page({ params }: { params: { id: string } }) {
     const [isLoading, setLoading] = useState(true)
 
     useEffect(() => {
-        fetch(`http://localhost:3000/api/page/${params.id}`)
+        fetch(`http://localhost:3000/api/course/${params.id}`)
             .then(res => res.json())
             .then(data => {
                 setData(data)
                 setLoading(false)
             })
-    
-}, [])
+    }, [])
 
 }
