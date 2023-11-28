d6434e2f426a13c134fcf6fddc90ef999aa215df
Zach <zacharyaclark@gmail.com>
2023-11-28 00:17:46 -0800

    started implementing searchable dropdown

--- website/src/app/rating/rateComponents.tsx
+++ website/src/app/rating/rateComponents.tsx
@@ -1,53 +1,43 @@
-import { useState, useEffect } from 'react';
 import React from 'react';
+import { Select } from 'antd';
 
-interface SearchableDropdownProps {
-    options: string[];
-}
+const onChange = (value: string) => {
+  console.log(`selected ${value}`);
+};
 
-function SearchableDropdown({ options }: SearchableDropdownProps) {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [filteredOptions, setFilteredOptions] = useState<string[]>(options);
+const onSearch = (value: string) => {
+  console.log('search:', value);
+};
 
-    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const searchTerm = e.target.value;
-        setSearchTerm(searchTerm);
+// Filter `option.label` match the user type `input`
+const filterOption = (input: string, option?: { label: string; value: string }) =>
+  (option?.label ?? '').toLowerCase().includes(input.toLowerCase());
 
-        // Filter options based on the search term
-        const filtered = options.filter((option: string) =>
-            option.toLowerCase().includes(searchTerm.toLowerCase())
-        );
-        setFilteredOptions(filtered);
-    };
+const SelectClass: React.FC = () => (
+    
 
-    const handleSelect = (selectedOption: string) => {
-        // Handle the selected option (e.g., pass it to a function, update state, etc.)
-        console.log('Selected:', selectedOption);
-    };
+  <Select
+    showSearch
+    placeholder="Select a class"
+    optionFilterProp=""
+    onChange={onChange}
+    onSearch={onSearch}
+    filterOption={filterOption}
+    options={[
+      {
+        value: 'jack',
+        label: 'Jack',
+      },
+      {
+        value: 'lucy',
+        label: 'Lucy',
+      },
+      {
+        value: 'tom',
+        label: 'Tom',
+      },
+    ]}
+  />
+);
 
-    return (
-        <div>
-            <input
-                type="text"
-                value={searchTerm}
-                onChange={handleSearch}
-                placeholder="Search for a class..."
-            />
-            <div>
-                {filteredOptions.length > 0 ? (
-                    <ul>
-                        {filteredOptions.map((option: string, index: number) => (
-                            <li key={index} onClick={() => handleSelect(option)}>
-                                {option}
-                            </li>
-                        ))}
-                    </ul>
-                ) : (
-                    <p>No matching classes found.</p>
-                )}
-            </div>
-        </div>
-    );
-}
-
-export default SearchableDropdown;
+export default SelectClass;
