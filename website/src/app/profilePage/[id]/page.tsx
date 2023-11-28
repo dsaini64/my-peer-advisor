2ab8aa4cefac6d44a3856a9925d01d9e8a0ac6fe
tmishra17 <jetmi@live.com>
2023-11-28 01:35:44 -0800

    added backend data to profile page

--- website/src/app/profilePage/[id]/page.tsx
+++ website/src/app/profilePage/[id]/page.tsx
@@ -1,27 +1,45 @@
 "use client";
 import { useState, useEffect } from "react";
-import { Button, Typography, } from "antd";
-const { Paragraph } = Typography
+import { Button } from "antd";
 
 type ProfilePageIdProps = {
   id: string
 }
 
+
+type ReviewType = {
+  id: string
+  professorID: string
+  courseID: {
+    id:string
+    classCode: string
+  }
+  rating: number
+  reviewType: string
+  likes: number
+  dislikes: number
+  comment: string
+  tags: [
+    {
+      id: string
+      tagName: string
+    }
+  ]
+  date: "string"
+  __v: number
+}
 export default function ProfilePage({id}:ProfilePageIdProps) {
   // call backend for data
-  id = "6563b27ff48eb1400c9f72d2"
+  id = "6563b27ff48eb14number0c9f72d2"
+  
   const [data, setData] = useState<any | null>(null)
-  let profName = "Michael Stravinsky"
-  let profDesc = "Background: Phd in Computer Science. Expert in Machine Learning and Artificial Intelligence. Worked for NASA."
-  let tags = "Caring, Inspirational, Funny"
-  let department  = "Computer Science"
-  let ratings = "32"
-  let ratingNum = "7.8"
-  let studentRatingNum = 5
-  let userRatingNum = ["9.0", "6.5", "8.3", "7.3"]
+  
   let userCourseName = "CSCI 187"
   let userDesc = "Umm Kulthum’s rise to fame was a very interesting journey. She was born on December 31st 1898, and was raised in a very small village in Egypt. Her family was very poor so she never had much growing up. The one thing she did had, which is also the thing that would carry her to fame, was her incredible voice. During the years Kulthum was growing up as a young women, most of the contemporary Arabic singers at the time were male. In fact, most of the singers she looked up to were male as well. Women played mostly a secondary role in regards to music, but things were much different for Kulthum. Her incredible voice made her stand out substantially from not only female singers but males singers as well. Many people who have heard Kulthum’s voice live, have commented on its beauty and fullness. Although gender roles were very much prominent during the years she was alive, it didn’t really seem to apply to her. Many people loved Umm Kulthum and her music."
   let userTags = "ready for class, lots of reading, caring, wants you to learn"
+  let profTags = []
+  let courses = []
+  
   useEffect(() => {
     fetch(`http://localhost:9080/api/v1/professors/${id}/reviews`)
     .then((response=> response.json()))
@@ -33,15 +51,36 @@ export default function ProfilePage({id}:ProfilePageIdProps) {
   }, [])
 
   console.log(data)
+  
+  if(data === null) return <p>failed to load data</p>
+  for (let i = 0; i < data.professor.tags.length; i++) {
+    if (data.professor.tags[i] && data.professor.tags[i]["tagName"]) {
+      profTags.push(data.professor.tags[i]["tagName"]);
+    }
+  }
+
+  for (let i = 0; i < data.professor.courses.length; i++) {
+    if (data.professor.courses[i] && data.professor.courses[i]["tagName"]) {
+      courses.push(data.professor.courses[i]["tagName"]);
+    }
+  }
+  console.log("profTags: ", profTags)
+  let ratingCount = data.professor.ratingCount
   return (
-    <div className="profilePageLayout" >
-      <ProfCard tags={tags} department={department} profDesc={profDesc} profName={profName} ratings={ratings} ratingNum={ratingNum} />
-      <div className="numOfUserReviews">{studentRatingNum} User Reviews</div>
+    <div className="profilePageLayout">
+      <ProfCard tags={profTags} department={data.professor.department} profDesc={data.professor.background} profName={data.professor.professorName} ratings={data.professor.ratingCount} ratingNum={data.professor.ratingTotal.toFixed(1)} />
+      <div className="numOfUserReviews">{ratingCount} {ratingCount > 1?
+        <>User Reviews</>
+        :
+        <>User Review</>
+      } </div>
       <div>
-        <UserCard userCourseName={userCourseName} userDesc={userDesc} userTags={userTags} ratingNum={userRatingNum[0]} />
-        <UserCard userCourseName={userCourseName} userDesc={profDesc} userTags={userTags} ratingNum={userRatingNum[1]} />
-        <UserCard userCourseName={userCourseName} userDesc={userDesc} userTags={userTags} ratingNum={userRatingNum[2]} />
-        <UserCard userCourseName={userCourseName} userDesc={userDesc} userTags={userTags} ratingNum={userRatingNum[3]} />
+          {data.reviews.map((key:ReviewType, i:number)=> 
+            // <UserCard key={i} userCourseName={key.courseID.classCode} userDesc={key.comment} userTags={key.tags} ratingNum={key.rating} />
+            <></>
+
+          )}
+       
       </div>
     </div>
   );  
@@ -51,24 +90,27 @@ type profileBodyProps = {
   profName: string,
   profDesc: string,
   department: string,
-  tags: string
+  tags: any[]
 }
 
 function ProfileBody(content: profileBodyProps) {
   return (
     <div className="profileBody">
-      <div><h1>{content.profName}</h1></div>
-      <div>{content.department}</div>
-      <div><p>{content.profDesc}</p></div> {/* display other courses taught as well */}
-      <div>Top Tags: {content.tags}</div> {/* tags is a list */}
+      <h1>{content.profName}</h1>
+      {content.department}
+      <p>Profile: <a href={content.profDesc}> {content.profDesc}</a></p> {/* display other courses taught as well */}
+      Top Tags: {content.tags.map(tag=> (
+            <>{tag}</>
+        ))}
+        {/* tags is a list */}
     </div>
   )
 }
 
 type profInfoProp = {
   profName: string,
-  reviewNum: string,
-  ratingNum: string
+  reviewNum: number,
+  ratingNum: number
 }
 
 function Rating({profName, reviewNum, ratingNum}: profInfoProp){
@@ -85,8 +127,8 @@ function Rating({profName, reviewNum, ratingNum}: profInfoProp){
 }
 
 type ratingHeaderProps = {
-  reviewNum: string,
-  ratingNum: string
+  reviewNum: number,
+  ratingNum: number
 }
 
 function RatingHeader({reviewNum, ratingNum}:ratingHeaderProps){
@@ -104,7 +146,7 @@ function RatingHeader({reviewNum, ratingNum}:ratingHeaderProps){
 }
 
 type userRatingProps = {
-  ratingNum: string
+  ratingNum: number
 }
 
 function UserRating({ratingNum}: userRatingProps) {
@@ -123,7 +165,7 @@ function UserRating({ratingNum}: userRatingProps) {
 
 type userBodyProps = {
   userDesc: string,
-  userTags: string,
+  userTags: any[],
   userCourseName: string
 }
 
@@ -146,10 +188,10 @@ function UserBody({userDesc, userTags, userCourseName}:userBodyProps) {
 type ProfCardProps = {
   profName: string,
   profDesc: string,
-  tags: string,
+  tags: any[]
   department: string,
-  ratings: string,
-  ratingNum: string
+  ratings: number,
+  ratingNum: number
 }
 
 function ProfCard({profName, ratings, ratingNum, department, tags, profDesc}:ProfCardProps) {
@@ -166,8 +208,8 @@ function ProfCard({profName, ratings, ratingNum, department, tags, profDesc}:Pro
 type UserCardProps = {
   userCourseName: string,
   userDesc: string,
-  userTags: string,
-  ratingNum: string
+  userTags: any[],
+  ratingNum: number
 }
 
 function UserCard({userCourseName, userDesc, userTags, ratingNum}: UserCardProps){
