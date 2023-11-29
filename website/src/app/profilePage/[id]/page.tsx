3686f789b65daef83a365a3c0ced04b15413979c
tmishra17 <jetmi@live.com>
2023-11-28 22:56:42 -0800

    fixed professor not found error

--- website/src/app/profilePage/[id]/page.tsx
+++ website/src/app/profilePage/[id]/page.tsx
@@ -4,100 +4,211 @@ import { Button } from "antd";
 import router from "next/router";
 
 
-type ReviewType = {
-  id: string
-  professorID: string
-  courseID: {
-    id:string
-    classCode: string
-  }
-  rating: number
-  reviewType: string
-  likes: number
-  dislikes: number
-  comment: string
-  tags: [
-    {
-      id: string
-      tagName: string
-    }
-  ]
-  date: "string"
-  __v: number
+// type ReviewType = {
+//   id: string
+//   professorID: string
+//   courseID: {
+//     id:string
+//     classCode: string
+//   }
+//   rating: number
+//   reviewType: string
+//   likes: number
+//   dislikes: number
+//   comment: string
+//   tags: [
+//     {
+//       id: string
+//       tagName: string
+//     }
+//   ]
+//   date: "string"
+//   __v: number
+// }
+// export default function ProfilePage({ params }: { params: { id: string } }) {
+//   // call backend for data
+
+//   const [data, setData] = useState<any | null>(null)
+//   useEffect(() => {
+//     fetch(`http://localhost:9080/api/v1/professors/${params.id}/reviews`)
+//     .then((response=> {
+//       if(response.ok) {
+//         console.log("response successful")
+//         return response.json()
+//       }
+//       else {
+//         console.error("Response Failed")
+//       }
+//     }
+    
+//     ))
+//     .then(data => {
+//       setData(data)
+//     })
+//     .catch(err=>console.log(err))
+//   }, [])
+
+//   console.log("data: ", data)
+
+  
+//   if(data === null) return <p>failed to load data</p>
+  
+//   //if(!(data.constructor === Array)) return <div>No Results found</div>
+  
+//   const tags: string[] = data?.professor.tags.map((item: { _id: string; tagName: string }) => (
+//     item.tagName
+//   ));
+  
+//   console.log("profTags: ", tags)
+//   let ratingCount = data?.professor?.ratingCount
+//   return (
+//     <div className="profilePageLayout">
+//       <ProfCard 
+//         tags={tags} 
+//         department={data?.professor.department} 
+//         profDesc={data?.professor.background} 
+//         profName={data?.professor.professorName} 
+//         ratings={data?.professor.ratingCount} 
+//         ratingNum={data?.professor.ratingTotal.toFixed(1)} 
+//         id={params.id} />
+//       <div className="numOfUserReviews">{ratingCount} {ratingCount > 1?
+//           <>User Reviews</>
+//         :
+//           <>User Review</>
+//       } </div>
+//       <div>
+//           {data.reviews.map((key: ReviewType, i: number)=> 
+//             <UserCard 
+//                 key={i} 
+//                 userCourseName={key.courseID.classCode} 
+//                 userDesc={key.comment} 
+//                 userTags={key.tags} 
+//                 ratingNum={key.rating} 
+//               />
+
+//           )}
+       
+//       </div>
+//     </div>
+//   );  
+// }
+
+export interface RootData {
+  professor?: Professor;
+  reviews?: ReviewType[];
 }
+
+export interface ReviewType {
+  rating: number;
+  tags: Tag[];
+  comment: string;
+  courseID: {
+    classCode: string;
+
+  };
+
+}
+
+export interface Professor {
+  professorName: string;
+  department: string;
+  background: string;
+  courses: Course[];
+  ratingCount: number;
+  ratingTotal: number;
+  tags: Tag[];
+  rating: number;
+}
+
+export interface Course {
+  courseName: string;
+  classCode: string;
+  description: string;
+  professors: string[];
+  ratingCount: number;
+  ratingTotal: number;
+  quarterAvailability: string[];
+  tags: Tag[];
+  rating: number;
+}
+
+export interface Tag {
+  tagName: string;
+  id: string;
+}
+
 export default function ProfilePage({ params }: { params: { id: string } }) {
   // call backend for data
+  const [data, setData] = useState<RootData>({})
 
-  const [data, setData] = useState<any | null>(null)
   useEffect(() => {
     fetch(`http://localhost:9080/api/v1/professors/${params.id}/reviews`)
-    .then((response=> {
-      if(response.ok) {
-        console.log("response successful")
-        return response.json()
-      }
-      else {
-        console.error("Response Failed")
-      }
-    }
-    
-    ))
+    .then((response => response.json()))
     .then(data => {
       setData(data)
+
     })
-    .catch(err=>console.log(err))
+    .catch(err => console.log(err))
   }, [])
 
   console.log("data: ", data)
 
-  
-  if(data === null) return <p>failed to load data</p>
-  
-  //if(!(data.constructor === Array)) return <div>No Results found</div>
-  
-  const tags: string[] = data?.professor.tags.map((item: { _id: string; tagName: string }) => (
-    item.tagName
-  ));
-  
+
+  if (data === null) return <p>failed to load data</p>
+
+  if ((Array.isArray(data))) return <div>No Results found</div>
+
+  let tags: string[] = []
+  const professor = data["professor"];
+  if (professor) {
+    if (professor.tags) {
+      tags = professor.tags.map(t => t.tagName);
+    }
+  }
+
+
   console.log("profTags: ", tags)
-  let ratingCount = data?.professor?.ratingCount
+  const ratingCount = data.professor?.ratingCount || 0;
   return (
     <div className="profilePageLayout">
-      <ProfCard 
-        tags={tags} 
-        department={data?.professor.department} 
-        profDesc={data?.professor.background} 
-        profName={data?.professor.professorName} 
-        ratings={data?.professor.ratingCount} 
-        ratingNum={data?.professor.ratingTotal.toFixed(1)} 
-        id={params.id} />
-      <div className="numOfUserReviews">{ratingCount} {ratingCount > 1?
-          <>User Reviews</>
+      <ProfCard
+        tags={tags}
+        department={data.professor?.department}
+        profDesc={data.professor?.background}
+        profName={data.professor?.professorName}
+        ratings={data.professor?.ratingCount}
+        ratingNum={data.professor && data.professor.ratingCount? data.professor.ratingTotal: 0}
+        id={params.id}
+      />
+      <div className="numOfUserReviews">{ratingCount} {ratingCount > 1 ?
+        <>User Reviews</>
         :
-          <>User Review</>
+        <>User Review</>
       } </div>
       <div>
-          {data.reviews.map((key: ReviewType, i: number)=> 
-            <UserCard 
-                key={i} 
-                userCourseName={key.courseID.classCode} 
-                userDesc={key.comment} 
-                userTags={key.tags} 
-                ratingNum={key.rating} 
-              />
+        {data.reviews?data.reviews.map((key: ReviewType, i: number) =>
+            <UserCard
+              key={i}
+              userCourseName={key.courseID.classCode}
+              userDesc={key.comment}
+              userTags={key.tags}
+              ratingNum={key.rating}
+            />
 
-          )}
-       
+        )
+        : null
+      }
+
       </div>
     </div>
-  );  
+  );
 }
 
 type profileBodyProps = {
-  profName: string,
-  profDesc: string,
-  department: string,
-  tags: any[]
+  profName?: string,
+  profDesc?: string,
+  department?: string,
+  tags: Tag[]
 }
 
 function ProfileBody(content: profileBodyProps) {
@@ -106,24 +217,21 @@ function ProfileBody(content: profileBodyProps) {
       <h1>{content.profName}</h1>
       {content.department}
       <p>Profile: <a href={content.profDesc}> {content.profDesc}</a></p> {/* display other courses taught as well */}
-      Top Tags: {content.tags.map(tag=> (
-            <>{tag}</>
-        ))}
-        {/* tags is a list */}
+      Top Tags: {content.tags.join(",")}
     </div>
   )
 }
 
 type profInfoProp = {
-  profName: string,
-  reviewNum: number,
-  ratingNum: number
-  id: string
+  profName?: string,
+  reviewNum?: number,
+  ratingNum?: number
+  id?: string
 }
 
 function Rating({profName, reviewNum, ratingNum, id}: profInfoProp){
   const ratingPage = () => {
-    router.push("/rating/professor/"+ id);
+    router.push("rating/professor/"+ id);
   }
   return (
     <div className="ratingLayout">
@@ -136,8 +244,8 @@ function Rating({profName, reviewNum, ratingNum, id}: profInfoProp){
 }
 
 type ratingHeaderProps = {
-  reviewNum: number,
-  ratingNum: number
+  reviewNum?: number,
+  ratingNum?: number
 }
 
 function RatingHeader({reviewNum, ratingNum}:ratingHeaderProps){
@@ -147,7 +255,10 @@ function RatingHeader({reviewNum, ratingNum}:ratingHeaderProps){
         Rating
       </div>
       <div className="ratingNumberText">
-        {ratingNum}
+        {!ratingNum ? 
+          <span>N/A</span>:
+          ratingNum.toFixed(1)
+      }
       </div>
       <div className="reviewNumText"> {reviewNum} ratings</div> 
     </div>
@@ -167,10 +278,10 @@ function UserRating({ratingNum}: userRatingProps) {
           </div>
           <div className="userRatingNumberText">
 
-            {!(ratingNum===null)?
-            ratingNum.toFixed(1)
-            :
+            {ratingNum===0?
               <>N/A</>
+            :
+              ratingNum.toFixed(1)
             }
           </div>
         </div>
@@ -179,15 +290,11 @@ function UserRating({ratingNum}: userRatingProps) {
 
 type userBodyProps = {
   userDesc: string,
-  userTags: [
-    {
-      id: string
-      tagName: string
-    }],
+  userTags: Tag[]
   userCourseName: string
 }
 
-function UserBody({userDesc, userTags, userCourseName}:userBodyProps) {
+function UserBody({userDesc, userTags, userCourseName}: userBodyProps) {
   
   return (
     <div className="userProfileBody">
@@ -198,31 +305,32 @@ function UserBody({userDesc, userTags, userCourseName}:userBodyProps) {
               <p>{userDesc}</p>
             }
       </div>
-      <div>Tags: {userTags && userTags.map(data => (
-            <span key={data.id}>
-              {data.tagName}
-            </span>
-      ))}
+      <div>Tags: {userTags && userTags.map(u=>u.tagName).join(", ")}
       </div>
     </div>
   )
 }
 
 type ProfCardProps = {
-  profName: string,
-  profDesc: string,
+  profName?: string,
+  profDesc?: string,
   tags: any[]
-  department: string,
-  ratings: number,
-  ratingNum: number
-  id: string
+  department?: string,
+  ratings?: number,
+  ratingNum?: number
+  id?: string
 }
 
 function ProfCard({profName, ratings, ratingNum, department, id, tags, profDesc}:ProfCardProps) {
   return (
     <div className="card">
     <div className="card-body profileCardLayout">
-        <Rating profName={profName} reviewNum={ratings} ratingNum={ratingNum} id={id}/>
+        <Rating 
+          profName={profName} 
+          reviewNum={ratings} 
+          ratingNum={ratingNum} 
+          id={id}
+        />
         <ProfileBody profName={profName} profDesc={profDesc} tags={tags} department={department}/>
     </div>
 </div>
@@ -232,10 +340,7 @@ function ProfCard({profName, ratings, ratingNum, department, id, tags, profDesc}
 type UserCardProps = {
   userCourseName: string,
   userDesc: string,
-  userTags: [{
-    id: string
-    tagName: string
-  }],
+  userTags: Tag[]
   ratingNum: number
 }
 
